0388cb959373201d1e4c85048fc2686443540395
mmtrying <mmtrying@gmail.com>
2020-05-21 18:42:20 +0800

    Update blogger-publish-post-function.js

--- blogger-publish-post-function.js
+++ blogger-publish-post-function.js
@@ -96,12 +96,13 @@ function staticpagepostwidget(json){
     runJsonInScript(json,staticPageSetting,'','',$pv);
     $pv.style.height= 'auto';
     $pv.style.overflow = 'visible';
-    window.scrollTo(0, 0);
+    //window.scrollTo(0, 0);
+    runNativeAds($pv);
     createStaticPagePagination(json);
   } else {
     var $pg = document.getElementById('staticfooter');
     if($pg) $pg.innerHTML = '<div id="pagination" class="nonepostwrapper"><div class="nonepostpager"><span>非常抱歉！沒有找到任何分享資料。如有不便，敬請原諒！</span></div><div class="nonepostpager"><a href="javascript:window.history.back();">&#9664;&#9664;&#32;&#36820;&#22238;&#19978;&#19968;&#38913;</a></div></div>';
-    window.scrollTo(0, 0);
+    //window.scrollTo(0, 0);
   }
 }
 
